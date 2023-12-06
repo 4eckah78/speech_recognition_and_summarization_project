aa0ae7cac98b3ff4345afe4603637b53ec078320
Tokareva Kseniia <airisfior05@duck.com>
2023-12-06 19:51:38 +0300

    Give the output file your own name

--- download_from_youtube.py
+++ download_from_youtube.py
@@ -1,20 +1,30 @@
 from pytube import YouTube
 
-def download_video(url, output_path='.'):
+def download_video(url, output_path='.', filename=None):
     try:
         # Создаем объект YouTube
         yt = YouTube(url)
 
+        # Получаем информацию о видео
+        video_title = yt.title
+
         # Выбираем поток с максимальным разрешением
         video_stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 
-        # Если вы хотите указать путь для сохранения файла, укажите output_path
-        video_stream.download(output_path)
+        # Составляем название файла
+        if filename:
+            file_name = filename
+        else:
+            file_name = video_title
 
-        print("Видео успешно скачано.")
+        # Если вы хотите указать путь для сохранения файла, укажите output_path
+        video_stream.download(output_path, filename=file_name)
+
+        print("Видео успешно скачано")
     except Exception as e:
         print(f"Произошла ошибка: {str(e)}")
 
 # Пример использования
-video_url = "https://www.youtube.com/watch?v=6gS1Bp4LZLc"#. "https://www.youtube.com/watch?v=ваш_идентификатор_видео"
-download_video(video_url, output_path='Youtube_videos')
+video_url = "https://www.youtube.com/watch?v=6gS1Bp4LZLc"  #. "https://www.youtube.com/watch?v=ваш_идентификатор_видео"
+custom_filename = "пользовательское_название.mp4"
+download_video(video_url, output_path='Youtube_videos', filename=custom_filename)
